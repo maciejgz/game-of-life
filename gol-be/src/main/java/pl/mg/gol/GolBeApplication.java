8a4af4a7d15898c1ed96ed832d858615c00e6a0f
maciejgz <macgzi@gmail.com>
2022-08-04 09:47:33 +0200

    modules initialization

---
+++ gol-be/src/main/java/pl/mg/gol/GolBeApplication.java
@@ -0,0 +1,13 @@
+package pl.mg.gol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GolBeApplication.class, args);
+    }
+
+}
---
+++ gol-be/src/test/java/pl/mg/gol/GolBeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mg.gol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
